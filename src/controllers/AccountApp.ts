b120aa55bb59672b75f5a3767ca71c6aa2f6f602
if1live <libsora25@gmail.com>
2024-09-18 23:50:09 +0900

    에러 터지면 대충 로그 찍고 정지. 있던 db 날리지 않도록 주의

--- src/controllers/AccountApp.ts
+++ src/controllers/AccountApp.ts
@@ -32,16 +32,41 @@ router.get("/current/s8202/:accountIndex", async (c) => {
 
 // update: db 수동 갱신
 router.post("/update", async (c) => {
-  const accounts = R.range(1, settings.ACCOUNT_COUNT + 1);
-  const tasks = accounts.map(async (accountIndex) =>
-    NamuhClient.fetch_s8202(accountIndex),
-  );
-  const results = await Promise.all(tasks);
-
   const now = new Date();
   const dateKst = deriveDateKst(now);
-  await BalanceService.save(db, results, dateKst);
-  return c.json({ now, dateKst });
+
+  try {
+    const accounts = R.range(1, settings.ACCOUNT_COUNT + 1);
+    const tasks = accounts.map(async (accountIndex) =>
+      NamuhClient.fetch_s8202(accountIndex),
+    );
+    const results = await Promise.all(tasks);
+
+    // TODO: 에러는 실제로 터지는거 보고 대응할 예정
+    // 로그인 에러가 발생할지도?
+    for (const result of results) {
+      if (result.errors.length > 0) {
+        for (const error of result.errors) {
+          console.error(error);
+          throw new Error("Error occurred while fetching s8202");
+        }
+      }
+    }
+
+    await BalanceService.save(db, results, dateKst);
+    return c.json({ now, dateKst });
+  } catch (e) {
+    console.error(e);
+    const data =
+      e instanceof Error
+        ? {
+            name: e.name,
+            message: e.message,
+            stack: e.stack?.split("\n"),
+          }
+        : (e as object);
+    return c.json(data, 500);
+  }
 });
 
 // recent: db 기준으로 조회할것
