fe23121ab8a3d848437d1becb6428b8c65c2de5e
if1live <libsora25@gmail.com>
2024-09-18 23:18:53 +0900

    현재 잔고 갱신은 날짜 기준으로 통째로 날리고 새로 추가

--- src/repositories/DailyHoldingRepository.ts
+++ src/repositories/DailyHoldingRepository.ts
@@ -1,14 +1,25 @@
 import { type MyKysely, DailyHoldingTable as Table } from "../tables/index.js";
 
 export const upsert = async (db: MyKysely, rows: Table.NewRow[]) => {
-  if (rows.length === 0) {
+  return await db.transaction().execute(async (trx) => {
+    return upsert_naive(trx, rows);
+  });
+};
+
+const upsert_naive = async (db: MyKysely, rows: Table.NewRow[]) => {
+  const first = rows[0];
+  if (!first) {
     return;
   }
+
+  // upsert로 싸우기 싫어서 기존 항목 지우고 추가
+  // 어차피 날짜 단위로 통째로 갱신이니까 큰 문제 없을듯?
+  const dateKst = first.date_kst;
+  await db.deleteFrom(Table.name).where("date_kst", "=", dateKst).execute();
 
   const result = await db
     .insertInto(Table.name)
     .values(rows)
-    .onConflict((oc) => oc.columns(Table.primaryKey).doNothing())
     .executeTakeFirst();
   return result;
 };
