5f7df403adf34774725fca5be1c5df9cefcdb5f8
if1live <libsora25@gmail.com>
2024-09-20 00:09:38 +0900

    favicon, robots.txt

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
+import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-
 import { logger } from "hono/logger";
 import { AccountApp, StatusApp } from "./controllers/index.js";
 import { engine } from "./instances/engine.js";
@@ -8,6 +8,17 @@ import { engine } from "./instances/engine.js";
 export const app = new Hono();
 app.use(logger());
 app.use("*", cors());
+
+const robotsTxt = `
+User-agent: *
+Disallow: /
+`.trimStart();
+
+app.get("/robots.txt", async (c) => {
+  return c.text(robotsTxt);
+});
+
+app.use("/static/*", serveStatic({ root: "./" }));
 
 app.get("/", async (c) => {
   const html = engine.renderFile("index");
